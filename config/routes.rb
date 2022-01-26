15b5b1efead21241402ef7e3c8351efbda2e9bb1
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-01-26 20:58:12 +0530

    Custom routes directs to customized controllers.
    Use tailwind to modify devise related views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
-  namespace :cms do
-    resources :posts do
-      resources :documents, only: %i[index create destroy], controller: 'post_documents'
+  devise_for :admins, skip: %i[registrations], controllers: {
+    sessions: "accounts/admins/sessions",
+    passwords: "accounts/admins/passwords",
+    confirmations: "accounts/admins/confirmations",
+    unlocks: "accounts/admins/unlocks"
+  }
+
+  scope module: "accounts" do
+    devise_scope :admin do
+      get "/admins/edit",
+        to: "admins/registrations#edit",
+        as: "edit_admin_registration"
+
+      match "/admins",
+        to: "admins/registrations#update",
+        as: "admin_registration",
+        via: %i[put patch]
     end
   end
+
+  namespace :cms do
+    resources :posts do
+      resources :documents, only: %i[index create destroy], controller: "post_documents"
+    end
+  end
+
+  root to: "cms/posts#index"
 end
