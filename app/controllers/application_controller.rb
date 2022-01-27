535b7f7fd56443e46f0774c8a71e2f6f7e6076bd
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-01-27 23:32:51 +0530

    Set after_sign_up_path_for for Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      management_root_path
+    else
+      root_path
+    end
+  end
 end
