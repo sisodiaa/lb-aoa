9272e1fdf35bf1533102f7314e2683ee40883e6c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-01-31 20:14:14 +0530

    Create dashboard for posts

--- app/controllers/management/dashboard_controller.rb
+++ app/controllers/management/dashboard_controller.rb
@@ -2,6 +2,9 @@ module Management
   class DashboardController < ApplicationController
     before_action :authenticate_admin!
 
-    def index; end
+    def index
+      @drafts = Post.draft.order(created_at: :asc)
+      @published_posts = Post.published.order(published_at: :desc)
+    end
   end
 end
