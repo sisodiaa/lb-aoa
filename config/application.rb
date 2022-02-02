38400b33fece44805c0460eec93104c0fd7ee86c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-02-02 17:34:04 +0530

    Set time zone of application to New Delhi

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module LbAoa
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "New Delhi"
   end
 end
