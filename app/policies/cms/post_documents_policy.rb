4104affaaf3fa675541cb0468f0deded036b10b5
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-03-29 19:17:10 +0530

    Remove unused code from cms policies

--- app/policies/cms/post_documents_policy.rb
+++ app/policies/cms/post_documents_policy.rb
@@ -1,10 +1,4 @@
 class CMS::PostDocumentsPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
-
   def create?
     record.draft?
   end
--- app/policies/cms/post_policy.rb
+++ app/policies/cms/post_policy.rb
@@ -1,10 +1,4 @@
 class CMS::PostPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
-
   def update?
     record.draft?
   end
