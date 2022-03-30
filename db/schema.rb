8fc4ac1565ec5efa634ccc01db852cf93c58f907
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-03-30 16:57:41 +0530

    Add new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_051225) do
+ActiveRecord::Schema.define(version: 2022_03_30_112031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
