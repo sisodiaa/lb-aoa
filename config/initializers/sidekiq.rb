f28b44e4e4a2ac59d6ec9b2f4a5ec5e01a266b5c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-04-02 11:01:34 +0530

    Verify mode for SSL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,12 @@
+SIDEKIQ_REDIS_CONFIGURATION = {
+  url: ENV["REDIS_URL"],
+  ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+}
+
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")}
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")}
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
