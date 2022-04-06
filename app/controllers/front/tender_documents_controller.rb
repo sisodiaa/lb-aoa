02beff8827572205cca90e3beb10f711cbb4b76c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-04-06 16:46:30 +0530

    Rectify front's tender documents controller

--- app/controllers/front/tender_documents_controller.rb
+++ app/controllers/front/tender_documents_controller.rb
@@ -1,27 +1,10 @@
 module Front
-  class TendersController < ApplicationController
+  class TenderDocumentsController < ApplicationController
     layout "front"
 
     def index
-      @page = params[:page] || 1
-      @status = params[:status] || "upcoming"
-    end
-
-    def tenders
-      @status = params[:status]
-      @pagy, @tenders = pagy(published_tenders.order(published_at: :desc), items: 6)
-    end
-
-    def show
-      @tender = Tender.find(params[:id])
-    end
-
-    private
-
-    def published_tenders
-      Tender.published.try!(params[:status].to_sym)
-    rescue NoMethodError
-      Tender.none
+      @tender = Tender.find(params[:tender_id])
+      @documents = @tender.documents.with_attached_file
     end
   end
 end
