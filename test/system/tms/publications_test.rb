64bf80c6c0a320ba8b20cc1a7ea160237788eadc
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-04-06 15:23:35 +0530

    Add views for TMS Tender publication

---
+++ test/system/tms/publications_test.rb
@@ -0,0 +1,66 @@
+require "application_system_test_case"
+
+module TMS
+  class PublicationsTest < ApplicationSystemTestCase
+    setup do
+      Warden.test_mode!
+      @confirmed_board_admin = admins(:confirmed_board_admin)
+      @draft_tender = tenders(:cctv_cables)
+    end
+
+    teardown do
+      @confirmed_board_admin = @draft_tender = nil
+      Warden.test_reset!
+    end
+
+    test "publishing a tender" do
+      authenticated_admin do
+        visit tms_tender_url(@draft_tender)
+        click_button "Publish Tender"
+        assert_selector "[role='toast']", text: "Tender was successfully published."
+      end
+    end
+
+    test "can not publish a tender with closing date before opening date" do
+      @draft_tender.opening = @draft_tender.closing + 3.days
+      @draft_tender.save
+
+      authenticated_admin do
+        visit tms_tender_url(@draft_tender)
+        click_button "Publish Tender"
+        assert_selector :css, "[role='toast']", text: "Tender was not published."
+      end
+    end
+
+    test "can not publish a tender with opening date before current date" do
+      @draft_tender.opening = DateTime.current - 3.days
+      @draft_tender.save
+
+      authenticated_admin do
+        visit tms_tender_url(@draft_tender)
+        click_button "Publish Tender"
+        assert_selector :css, "[role='toast']", text: "Tender was not published."
+      end
+    end
+
+    test "can not publish a tender with opening date and closing date before current date" do
+      @draft_tender.opening = DateTime.current - 3.days
+      @draft_tender.closing = DateTime.current - 2.days
+      @draft_tender.save
+
+      authenticated_admin do
+        visit tms_tender_url(@draft_tender)
+        click_button "Publish Tender"
+        assert_selector :css, "[role='toast']", text: "Tender was not published."
+      end
+    end
+
+    private
+
+    def authenticated_admin
+      login_as @confirmed_board_admin, scope: :admin
+      yield if block_given?
+      logout :admin
+    end
+  end
+end
