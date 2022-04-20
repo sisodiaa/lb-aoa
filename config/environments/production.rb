1c238c39fea4613001ff0ecfb88c4d1daaf587a5
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-04-20 21:03:26 +0530

    Replace ActionMailer::Base.smtp_settings with config.action_mailer.smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: "https://www.lbaoa.com"}
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     user_name: "apikey",
     password: Rails.application.credentials.dig(:sendgrid, :api_key),
     domain: Rails.application.credentials.dig(:sendgrid, :domain),
