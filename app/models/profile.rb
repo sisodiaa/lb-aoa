2fda3798bc8a5735c6e7da18e84af9650a92fa29
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-08-23 10:57:00 +0530

    Allow profile creation with blank phone number

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :phone_number, phone: true
+  validates :phone_number, phone: {allow_blank: true}
 
   def full_name
     "#{first_name} #{middle_name} #{last_name}".titleize
