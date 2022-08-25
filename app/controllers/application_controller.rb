03cd77d442e152d299189e88e42dfd80cd318d9d
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-08-25 11:30:11 +0530

    Add sign out functionality for Owners

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       management_root_path
+    else
+      root_path
     end
   end
 
