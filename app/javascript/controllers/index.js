382f331d0b6136920a82a473991e13de23cf9a36
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-03 00:39:22 +0530

    Remove Dashboard's component stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,41 +4,38 @@
 
 import { application } from "./application"
 
-import Cms__EditorController from "./cms/editor_controller.js"
+import Cms__EditorController from "./cms/editor_controller"
 application.register("cms--editor", Cms__EditorController)
 
-import Cms__VisibilityToggleController from "./cms/visibility_toggle_controller.js"
+import Cms__VisibilityToggleController from "./cms/visibility_toggle_controller"
 application.register("cms--visibility-toggle", Cms__VisibilityToggleController)
 
-import Dashboard__ComponentController from "./dashboard/component_controller.js"
-application.register("dashboard--component", Dashboard__ComponentController)
-
-import Dashboard__ListController from "./dashboard/list_controller.js"
+import Dashboard__ListController from "./dashboard/list_controller"
 application.register("dashboard--list", Dashboard__ListController)
 
-import ElementRemovalController from "./element_removal_controller.js"
+import ElementRemovalController from "./element_removal_controller"
 application.register("element-removal", ElementRemovalController)
 
-import Flowbite__CollapseController from "./flowbite/collapse_controller.js"
+import Flowbite__CollapseController from "./flowbite/collapse_controller"
 application.register("flowbite--collapse", Flowbite__CollapseController)
 
-import Flowbite__DropdownController from "./flowbite/dropdown_controller.js"
+import Flowbite__DropdownController from "./flowbite/dropdown_controller"
 application.register("flowbite--dropdown", Flowbite__DropdownController)
 
-import Form__SubmissionController from "./form/submission_controller.js"
+import Form__SubmissionController from "./form/submission_controller"
 application.register("form--submission", Form__SubmissionController)
 
-import HelloController from "./hello_controller.js"
+import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import Owners__Dashboards__TabController from "./owners/dashboards/tab_controller.js"
+import Owners__Dashboards__TabController from "./owners/dashboards/tab_controller"
 application.register("owners--dashboards--tab", Owners__Dashboards__TabController)
 
-import Search__Post__ModalController from "./search/post/modal_controller.js"
+import Search__Post__ModalController from "./search/post/modal_controller"
 application.register("search--post--modal", Search__Post__ModalController)
 
-import Search__Post__SelectOptionController from "./search/post/select_option_controller.js"
+import Search__Post__SelectOptionController from "./search/post/select_option_controller"
 application.register("search--post--select-option", Search__Post__SelectOptionController)
 
-import ToastController from "./toast_controller.js"
+import ToastController from "./toast_controller"
 application.register("toast", ToastController)
