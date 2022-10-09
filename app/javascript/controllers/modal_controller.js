bdab3b40f9074632705233e53ab82c0bf6a213b4
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-09 18:32:01 +0530

    Modify references to Modal stimulus controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="search--post--modal"
+// Connects to data-controller="modal"
 export default class extends Controller {
   static targets = ["modal"];
 
