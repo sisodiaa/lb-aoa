01d25df566cb10c3b9815d4198917f3c608d71eb
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-09 16:52:56 +0530

    Simplify modal based posts' search interactions
    Remove modal only when 'Cancel' is clicked, and semi migrate logic for
    preventDefault and removing modal on successful submission to html
    through data-* attributes

--- app/javascript/controllers/search/post/modal_controller.js
+++ app/javascript/controllers/search/post/modal_controller.js
@@ -2,32 +2,10 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search--post--modal"
 export default class extends Controller {
-  static targets = ["modal", "form"];
+  static targets = ["modal"];
 
-  connect() {
-    document.addEventListener("turbo:before-visit", () => {
-      this.removeModal();
-    });
-  }
-
-  hideModal(e) {
-    if (e.type === "click") {
-      e.preventDefault();
-    }
-
-    this.removeModal();
-  }
-
-  removeModal(e) {
-    if (
-      e &&
-      (this.formTarget.contains(e.target) ||
-        this.formTarget.parentElement.contains(e.target))
-    ) {
-      return;
-    }
-
-    if (this.hasModalTarget) {
+  removeModal(event) {
+    if (event.params.dismiss || event.detail.success) {
       this.modalTarget.parentElement.removeAttribute("src");
       this.modalTarget.remove();
     }
