676099002092db56f5beb904cc63aa9768cafb65
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-15 18:29:12 +0530

    Add SVG icon component form Home

---
+++ test/components/icon/outline/home_component_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Icon::Outline::HomeComponentTest < ViewComponent::TestCase
+  test "that component renders outline icon for home" do
+    render_inline Icon::Outline::HomeComponent.new(stroke_width: 2.5, classes: "w-5 h-5")
+    assert_selector "svg[fill='none'][viewbox='0 0 24 24'][stroke-width='2.5'][stroke='currentColor'][class='w-5 h-5']"
+  end
+end
