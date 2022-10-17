2df247521640beb8911f3f18ab8a86d4033a6236
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-18 00:43:34 +0530

    Create a default button component

---
+++ app/components/button/default_button_component.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+class Button::DefaultButtonComponent < ViewComponent::Base
+  attr_reader :type, :variant, :size, :block
+
+  def initialize(type: :button, variant: :primary, size: :base, icon_position: :left, block: false)
+    @type = type
+    @variant = variant
+    @size = size
+    @icon_position = icon_position
+    @block = block
+  end
+
+  def before_render
+    @class_name = [block_class, size_class, variant_class].reject(&:empty?).join(" ")
+  end
+
+  renders_one :text
+  renders_one :icon
+
+  private
+
+  def block_class
+    return "w-full" if block
+    ""
+  end
+
+  def size_class
+    return "px-3 py-1.5 text-xs" if size == :small
+    "px-5 py-2.5 text-sm"
+  end
+
+  def variant_class
+    case variant
+    when :secondary
+      secondary_variant_class
+    when :success
+      success_variant_class
+    when :error
+      error_variant_class
+    else
+      primary_variant_class
+    end
+  end
+
+  def primary_variant_class
+    "bg-lb-500 hover:bg-lb-600 active:bg-lb-700 " \
+    "shadow-sm shadow-lb-100 hover:shadow-xs hover:shadow-lb-50 active:shadow-none"
+  end
+
+  def secondary_variant_class
+    "bg-blue-700 hover:bg-blue-800 active:bg-blue-900 " \
+    "shadow-sm shadow-blue-300 hover:shadow-xs hover:shadow-blue-100 active:shadow-none"
+  end
+
+  def success_variant_class
+    "bg-emerald-600 hover:bg-emerald-700 active:bg-emerald-800 " \
+    "shadow-sm shadow-emerald-200 hover:shadow-xs hover:shadow-emerald-100 active:shadow-none"
+  end
+
+  def error_variant_class
+    "bg-red-600 hover:bg-red-700 active:bg-red-800 " \
+    "shadow-sm shadow-red-200 hover:shadow-xs hover:shadow-red-100 active:shadow-none"
+  end
+end
---
+++ test/components/button/default_button_component_test.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Button::DefaultButtonComponentTest < ViewComponent::TestCase
+  test "that component generates a button with text and icon" do
+    render_inline(Button::DefaultButtonComponent.new) do |component|
+      component.with_text { "Hello" }
+      component.with_icon { "Icon" }
+    end
+
+    assert_selector "button.inline-flex.text-white[type=\"button\"]", text: "Icon\n\n    \n      Hello"
+  end
+
+  test "that component generates a button with text only" do
+    render_inline Button::DefaultButtonComponent.new do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_no_selector "button.w-full[type=\"button\"]"
+    assert_selector "button.inline-flex.text-white.px-5.text-sm.bg-lb-500[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a button with icon only" do
+    render_inline Button::DefaultButtonComponent.new do |c|
+      c.with_icon { "Icon" }
+    end
+
+    assert_selector "button.inline-flex.text-white[type=\"button\"]", text: "Icon"
+  end
+
+  test "that component generates a block button" do
+    render_inline Button::DefaultButtonComponent.new(block: true) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button.w-full[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a small button" do
+    render_inline Button::DefaultButtonComponent.new(size: :small) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button.px-3.text-xs[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a secondary variant button" do
+    render_inline Button::DefaultButtonComponent.new(variant: :secondary) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button.bg-blue-700[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a success variant button" do
+    render_inline Button::DefaultButtonComponent.new(variant: :success) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button.bg-emerald-600[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a error variant button" do
+    render_inline Button::DefaultButtonComponent.new(variant: :error) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button.bg-red-600[type=\"button\"]", text: "Hello"
+  end
+
+  test "that component generates a button with type `submit`" do
+    render_inline Button::DefaultButtonComponent.new(type: :submit) do |c|
+      c.with_text { "Hello" }
+    end
+
+    assert_selector "button[type=\"submit\"]", text: "Hello"
+  end
+end
