cd64ebe0f9dd3d524e327e0d752972f3d69b8a31
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-10-26 19:38:58 +0530

    Extend search to include first name and last name
    Search by first name and last name is case insensitive, modify search by
    email to be case insensitive.

--- app/forms/management/search/owners_form.rb
+++ app/forms/management/search/owners_form.rb
@@ -3,20 +3,37 @@ module Management
     class OwnersForm
       include ActiveModel::API
 
-      attr_accessor :email
+      attr_accessor :email, :first_name, :last_name
 
       def initialize(attributes = {})
         @email = attributes.fetch(:email, nil)
+        @first_name = attributes.fetch(:first_name, nil)
+        @last_name = attributes.fetch(:last_name, nil)
       end
 
-      validate :email_address
+      validate :owner_details
 
-      def email_address
-        errors.add(:email, "can not be blank") if email == ""
+      def owner_details
+        errors.add(:base, "Email address, First Name, or Last Name is required") if details_missing?
       end
 
       def search
-        Owner.where(email: email)
+        results = Owner.includes(:profile)
+        results = results.where(email: email.downcase) if email.present?
+
+        if first_name.present?
+          results = results.where("profiles.first_name ILIKE ?", first_name).references(:profiles)
+        end
+
+        if last_name.present?
+          results = results.where("profiles.last_name ILIKE ?", last_name).references(:profiles)
+        end
+
+        results
+      end
+
+      def details_missing?
+        email.blank? && first_name.blank? && last_name.blank?
       end
     end
   end
--- test/forms/management/search/owners_form_test.rb
+++ test/forms/management/search/owners_form_test.rb
@@ -7,7 +7,9 @@ module Management
 
       setup do
         @model = Management::Search::OwnersForm.new({
-          email: "owner_two@example.com"
+          email: "owner_two@example.com",
+          first_name: "Second",
+          last_name: "Owner"
         })
       end
 
@@ -15,11 +17,37 @@ module Management
         @model = nil
       end
 
-      test "email is present" do
+      test "that email, or first name, or last name is present" do
         @model.email = ""
+        @model.first_name = ""
+        @model.last_name = ""
 
         assert_not @model.valid?
-        assert_equal ["can not be blank"], @model.errors[:email]
+        assert_equal ["Email address, First Name, or Last Name is required"], @model.errors[:base]
+      end
+
+      test "search if only email is given" do
+        @model.first_name = ""
+        @model.last_name = ""
+
+        assert_equal 1, @model.search.count
+        assert_includes @model.search, Owner.find_by(email: "owner_two@example.com"), "Result not found"
+      end
+
+      test "search if only first name is given" do
+        @model.email = ""
+        @model.last_name = ""
+
+        assert_equal 1, @model.search.count
+        assert_includes @model.search, Owner.find_by(email: "owner_two@example.com"), "Result not found"
+      end
+
+      test "search if only last name is given" do
+        @model.email = ""
+        @model.first_name = ""
+
+        assert_equal 7, @model.search.count
+        assert_equal Owner.all.pluck(:id), @model.search.pluck(:id), "Results not found"
       end
     end
   end
