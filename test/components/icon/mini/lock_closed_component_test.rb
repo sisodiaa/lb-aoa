7c922b8a4ecde11049b552770ce130216d1ee0d7
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-11-07 19:45:47 +0530

    Create icon component for Closed Lock

---
+++ test/components/icon/mini/lock_closed_component_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Icon::Mini::LockClosedComponentTest < ViewComponent::TestCase
+  test "that component renders mini icon for closed lock" do
+    render_inline Icon::Mini::LockClosedComponent.new(fill: "currentColor", view_box: "0 0 20 20", classes: "w-5 h-5")
+    assert_selector "svg[fill='currentColor'][viewbox='0 0 20 20'][class='w-5 h-5']"
+    assert_no_selector "svg[stroke][stroke-width]"
+  end
+end
