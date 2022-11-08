716874aa51630bcde36b39831cfb6e08bc277eb4
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-11-08 20:13:04 +0530

    Add title attribute to the icon

--- app/components/icon/mini/lock_closed_component.rb
+++ app/components/icon/mini/lock_closed_component.rb
@@ -2,12 +2,14 @@
 
 class Icon::Mini::LockClosedComponent < ViewComponent::Base
   def initialize(
+    title: "Closed Lock",
     fill: "none",
     view_box: "0 0 24 24",
     stroke_width: 1.5,
     stroke: "currentColor",
     classes: "w-6 h-6"
   )
+    @title = title
     @fill = fill
     @view_box = view_box
     @stroke_width = stroke_width
--- test/components/icon/mini/lock_closed_component_test.rb
+++ test/components/icon/mini/lock_closed_component_test.rb
@@ -4,8 +4,11 @@ require "test_helper"
 
 class Icon::Mini::LockClosedComponentTest < ViewComponent::TestCase
   test "that component renders mini icon for closed lock" do
-    render_inline Icon::Mini::LockClosedComponent.new(fill: "currentColor", view_box: "0 0 20 20", classes: "w-5 h-5")
+    render_inline Icon::Mini::LockClosedComponent.new(
+      title: "Locked Discussion", fill: "currentColor", view_box: "0 0 20 20", classes: "w-5 h-5"
+    )
     assert_selector "svg[fill='currentColor'][viewbox='0 0 20 20'][class='w-5 h-5']"
     assert_no_selector "svg[stroke][stroke-width]"
+    assert_selector "svg title", text: "Locked Discussion"
   end
 end
