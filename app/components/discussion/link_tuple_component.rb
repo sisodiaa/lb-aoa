39345fd7cfab3cf9eb30430893b12881d41108d7
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2022-11-17 17:30:24 +0530

    Accept data attributes

--- app/components/discussion/link_tuple_component.rb
+++ app/components/discussion/link_tuple_component.rb
@@ -4,10 +4,11 @@ class Discussion::LinkTupleComponent < ViewComponent::Base
   renders_one :icon
   renders_one :text
 
-  def initialize(class_name: "", path: "#", renderable: true)
+  def initialize(class_name: "", path: "#", renderable: true, data: {})
     @path = path
     @class_name = class_name
     @renderable = renderable
+    @data = data
   end
 
   def render?
